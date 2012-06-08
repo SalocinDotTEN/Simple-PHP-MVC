cbbf308f8c69f5b9a93f0f7a31e9809626eab3db
Salocin.TEN <salocin.ten@gmail.com>
2012-06-08 15:09:07 +0800

    Rip out Cassandra stuff.

--- include/conf.inc.php
+++ include/conf.inc.php
@@ -1,42 +1,12 @@
 <?php
 	/*
-		Cassandra Cluster Admin
-		
-		@author Sébastien Giroux
-		@copyright All rights reserved - 2011
+		Based on Cassandra Cluster Admin by Sébastien Giroux
 	*/
-
-	/*
-		Add as many Cassandra nodes as you want in the nodes array like so: array('10.70.80.90:9160','10.70.80.100:9160','10.70.80.110:9160')
-		
-		Multi cluster is also supported:
-		
-		$CASSANDRA_CLUSTERS = array(array('nodes' => array('127.0.0.1:9160'),
-										 'username' => '',
-										 'password' => ''),
-								   array('nodes' => array('10.12.13.14:9160','10.13.12.14:9160'),
-										 'username' => '',
-										 'password' => ''));
-	*/
-	
-	$CASSANDRA_CLUSTERS = array(array('nodes' => array('127.0.0.1:9160'),
-								     'username' => '',
-								     'password' => ''));
 	
 	/*
-		Read-only keyspace
-		
-		Some keyspace can be set to read-only so nothing can be added/edited/deleted from them.
+		Configure the login information.
 	*/
-	
-	define('READ_ONLY_KEYSPACES','system'); // Seperate by comma (,)
-	
-	/*
-		Enter your Cassandra Cluster Admin credentials here
-	*/
-	define('CCA_LOGIN_REQUIRED',false);
-	define('CCA_USERNAME','root'); 	
-	define('CCA_PASSWORD','');
-	
-	define('MX4J_HTTP_ADAPTOR_PORT',8081);
+	define('CCA_LOGIN_REQUIRED',false); //If logging in is required for this app.
+	define('CCA_USERNAME','root'); 	//Login user name.
+	define('CCA_PASSWORD',''); //Login password
 ?>
